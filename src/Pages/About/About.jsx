c893b3991c2e9e4042957517a50741b7db735199
SyedYaamin1604 <Syedyaamin1604@gmail.com>
2025-02-17 13:58:15 +0500

    Update 4.0

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -16,19 +16,19 @@ import { FaGitAlt } from "react-icons/fa6";
 const About = () => {
   return (
     <>
-      <div className='flex flex-col lg:flex-row lg:justify-between lg:pt-20 lg:mx-12 xl:mx-28 items-center'>
+      <div className='flex flex-col lg:flex-row lg:justify-between px-4 lg:pt-20 lg:mx-12 xl:mx-28 items-center'>
         <div className='mt-20 w-[200px] h-[200px] sm:w-[250px] sm:h-[250px] lg:w-[300px] lg:h-[300px] xl:w-[350px] xl:h-[350px]'>
           <img className='mt-10 w-[100%] h-[100%] rounded-full hover:shadow-2xl hover:shadow-blue-400 social-icon' src={image} alt="SyedYaamin" />
         </div>
         <div className='lg:mt-16'>
           <h1 className='text-blue-400 mt-20 text-center text-2xl md:text-3xl lg:text-4xl xl:text-5xl lg:text-left font-bold'>Who I Am?</h1>
-          <p className='text-white font-medium md:text-base lg:text-lg text-sm w-[32rem] sm:w-[35rem] md:w-[36rem] xl:w-[38rem] text-center mt-3 lg:text-left'>Hi, I'm Syed Yaamin, a dedicated software engineer in web development. I have a passion for building dynamic, responsive, and user-friendly web applications using JavaScript, React, and Firebase. I enjoy solving problems and working on projects that challenge me to learn and grow.</p>
-          <p className='text-white font-medium md:text-base lg:text-lg text-sm w-[32rem] sm:w-[35rem] md:w-[36rem] xl:w-[38rem] text-center mt-3 lg:text-left'>I'm always looking for opportunities to improve my skills and stay current with new technologies in the field. My aim is to contribute to meaningful projects and make a positive impact in the development industry.</p>
+          <p className='text-white text-xs font-medium md:text-base lg:text-lg min-[450px]:w-[28rem] sm:w-[35rem] md:w-[36rem] xl:w-[38rem] text-center mt-3 lg:text-left'>Hi, I'm Syed Yaamin, a dedicated software engineer in web development. I have a passion for building dynamic, responsive, and user-friendly web applications using JavaScript, React, and Firebase. I enjoy solving problems and working on projects that challenge me to learn and grow.</p>
+          <p className='text-white font-medium md:text-base lg:text-lg text-xs min-[450px]:w-[28rem] sm:w-[35rem] md:w-[36rem] xl:w-[38rem] text-center mt-3 lg:text-left'>I'm always looking for opportunities to improve my skills and stay current with new technologies in the field. My aim is to contribute to meaningful projects and make a positive impact in the development industry.</p>
         </div>
       </div>
       <div className='pb-20'>
         <h1 className='text-2xl md:text-3xl lg:text-4xl lg:mt-24 xl:text-5xl text-white font-bold text-center mt-10'>Professional <span className='text-blue-400'>Skillset</span></h1>
-        <div className='flex flex-wrap justify-center items-center gap-10 text-8xl text-blue-400 mt-10'>
+        <div className='flex flex-wrap justify-center items-center gap-8 text-8xl text-blue-400 mt-10'>
           <div className='w-[15rem] px-10 py-7 border-2 rounded-2xl flex flex-col justify-center items-center text-center border-blue-400 bg-black hover:bg-blue-400 hover:text-black hover:border-black social-icon'>
             <FaHtml5 />
             <span className='text-3xl font-semibold pt-3'>HTML</span>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -10,7 +10,7 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   const [textIndex, setTextIndex] = useState(0);
-  const text = ["Web Developer", "Front End Developer", "Wordpress Developer"];
+  const text = [" Web Developer", " Front End Developer", " Wordpress Developer"];
 
   useEffect(() => {
     const validInterval = setInterval(() => {
@@ -21,31 +21,37 @@ const Home = () => {
 
   return (
     <>
-      <div className="flex flex-col-reverse lg:flex-row lg:justify-between lg:mx-20 xl:mx-32 xl:mt-20 justify-center items-center mt-10 pb-10 custom-scrollbar">
+      <div className="flex flex-col-reverse lg:flex-row lg:justify-between lg:mx-20 xl:mx-32 xl:mt-20 justify-center items-center mt-10 lg:pt-5 px-2 sm:pb-10 custom-scrollbar">
         <div id="Left" className="text-white mt-10">
-          <h1 className="mt-5 text-2xl sm:text-3xl lg:text-4xl xl:text-5xl font-bold text-center lg:text-left">
+          <h1 className="text-xl min-[450px]:text-2xl sm:text-3xl lg:text-4xl xl:text-5xl font-bold text-center lg:text-left">
             Hi, I am{" "}
-            <span className="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl font-bold text-blue-400">
+            <span className="text-xl min-[450px]:text-2xl sm:text-3xl lg:text-4xl xl:text-5xl font-bold text-blue-400">
               Yaamin
             </span>
           </h1>
-          <h1 className="mt-2 xl:mt-5 text-center lg:text-left font-bold text-2xl sm:text-3xl lg:text-4xl xl:text-5xl">
+          <h1 className="mt-2 xl:mt-5 text-center lg:text-left font-bold text-xl min-[450px]:text-2xl sm:text-3xl lg:text-4xl xl:text-5xl">
             I am a{" "}
-            <span className="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl font-bold text-blue-400">
+            <span className="text-xl min-[450px]:text-2xl sm:text-3xl lg:text-4xl xl:text-5xl font-bold text-blue-400">
               {text[textIndex]}
             </span>
           </h1>
-          <p className="mt-2 text-center lg:text-left w-[27rem] sm:w-[30rem] lg:w-[32rem] xl:w-[40rem] font-normal lg:text-lg xl:text-xl xl:font-semibold xl:mt-8">
+          <p className="mt-2 text-center text-xs min-[450px]:text-sm w-full lg:text-left sm:w-[30rem] md:w-[32rem] lg:w-[35rem] xl:w-[40rem] font-normal md:text-base lg:text-lg xl:text-xl xl:font-semibold xl:mt-8">
             A frontend developer passionate about developing beautiful and
             functional websites. With expertise in HTML, CSS, JavaScript, and
             React, I can transform ideas into interactive web experiences. Let
             create something amazing together!
           </p>
-          <div className="mt-4 text-2xl sm:text-3xl lg:text-4xl xl:text-5xl lg:mt-6 xl:mt-8 flex justify-center lg:justify-start items-center gap-10">
-            <Link className="p-2 border-2 border-blue-400 bg-black text-blue-400 rounded-xl hover:border-black hover:text-black hover:bg-blue-400 social-icon" to="https://github.com/SyedYaamin1604">
-                <FaGithub />
+          <div className="mt-4 text-xl min-[450px]:text-2xl min-[450px]:gap-4 sm:text-3xl lg:text-4xl xl:text-5xl lg:mt-6 xl:mt-8 flex justify-center lg:justify-start items-center gap-2 sm:gap-5 md:gap-8 lg:gap-10">
+            <Link
+              className="p-2 border-2 border-blue-400 bg-black text-blue-400 rounded-xl hover:border-black hover:text-black hover:bg-blue-400 social-icon"
+              to="https://github.com/SyedYaamin1604"
+            >
+              <FaGithub />
             </Link>
-            <Link className="p-2 border-2 border-blue-400 bg-black text-blue-400 rounded-xl hover:border-black hover:text-black hover:bg-blue-400 social-icon" to="https://www.linkedin.com/in/syedyaamin/">
+            <Link
+              className="p-2 border-2 border-blue-400 bg-black text-blue-400 rounded-xl hover:border-black hover:text-black hover:bg-blue-400 social-icon"
+              to="https://www.linkedin.com/in/syedyaamin/"
+            >
               <FaLinkedin />
             </Link>
             <span className="p-2 border-2 border-blue-400 bg-black text-blue-400 rounded-xl hover:border-black hover:text-black hover:bg-blue-400 social-icon ">
@@ -58,26 +64,26 @@ const Home = () => {
               <MdEmail />
             </span>
           </div>
-          <div className="flex justify-center lg:justify-start items-center gap-5 mt-6 lg:mt-8 xl:mt-10">
+          <div className="flex justify-center lg:justify-start items-center gap-2 min-[450px]:gap-3 sm:gap-5 mt-6 lg:mt-8 xl:mt-10">
             <Link
-              className="py-3 px-5 text-md lg:text-lg xl:text-2xl font-semibold border-2 border-blue-400 rounded-full social-icon text-blue-400 bg-black hover:border-black hover:text-black hover:bg-blue-600"
+              className="py-2 px-3 lg:py-3 lg:px-5 text-sm min-[450px]:text-md sm:text-lg xl:text-2xl font-semibold border-2 border-blue-400 rounded-full social-icon text-blue-400 bg-black hover:border-black hover:text-black hover:bg-blue-600"
               to="/contact"
             >
               <button className="flex justify-center items-center gap-2">
                 Contact Me{" "}
-                <span className="text-xl xl:text-2xl">
+                <span className="text-md min-[450px]:text-lg sm:text-2xl">
                   <CgProfile />
                 </span>
               </button>
             </Link>
-            <button className="py-3 px-5 text-md lg:text-lg xl:text-2xl font-semibold border-2 border-black rounded-full social-icon bg-blue-600 text-black hover:bg-black hover:border-blue-400 hover:text-blue-400">
+            <button className="py-2 px-3 lg:py-3 lg:px-5 text-sm min-[450px]:text-md sm:text-lg xl:text-2xl font-semibold border-2 border-black rounded-full social-icon bg-blue-600 text-black hover:bg-black hover:border-blue-400 hover:text-blue-400">
               <a
                 href={Resume}
                 download="Resume.pdf"
                 className="flex justify-center items-center gap-2"
               >
-                Download Resume{" "}
-                <span className="text-xl xl:text-2xl">
+                Resume{" "}
+                <span className="text-md min-[450px]:text-lg sm:text-2xl">
                   <IoMdDownload />
                 </span>
               </a>
@@ -86,7 +92,7 @@ const Home = () => {
         </div>
         <div
           id="Right"
-          className="w-[250px] h-[250px] sm:w-[300px] sm:h-[300px] xl:w-[350px] xl:h-[350px] mt-10"
+          className="w-[200px] h-[200px] sm:w-[300px] sm:h-[300px] xl:w-[350px] xl:h-[350px] mt-10"
         >
           <img
             className="w-[100%] h-[100%] rounded-full social-icon hover:shadow-2xl hover:shadow-blue-400"
